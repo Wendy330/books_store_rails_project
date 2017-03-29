68469a8d56ad604406ffd02f5724e8a8200c65dc
Wendy330 <qian91328@gmail.com>
2017-03-29 18:22:47 +0000

    Add the model for About page

---
+++ app/admin/contact_page.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register ContactPage do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :title, :contentAddress
+actions :all, except: [:destroy, :new]
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328053016) do
+ActiveRecord::Schema.define(version: 20170329170545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20170328053016) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "contact_pages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "contentAddress"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "customers", force: :cascade do |t|
