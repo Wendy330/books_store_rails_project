ddca9fe0eadb3e5b669886b0e5f465e9a378a3ed
Wendy330 <qian91328@gmail.com>
2017-04-11 04:55:01 +0000

    Add bootstrap to project

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :initialize_session
+
   def index
     @products = Product.order("name").page(params[:page]).per(5)
     if params[:search]
@@ -15,4 +17,21 @@ class ProductsController < ApplicationController
   def by_category
     @products = Product.includes(:category).where(:category_id => params[:id])
   end
+
+  def add_to_cart
+    id = params[:id].to_i
+    session[:shopping_cart_list] << id unless session[:shopping_cart_list].include?(id)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def mark_as_added
+
+  end
+
+  private
+  def initialize_session
+    session[:shopping_cart_list] ||= []
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'about' => 'about_page#index', as: 'about'
-  get 'contact' => 'contact_page#index', as: 'contact'
-
-  get ':id' => 'products#by_category', as: 'categories'
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources 'products', only: [:index, :show]
+  resources 'products', only: [:index, :show] do
+    member do
+      post :add_to_cart
+      post :mark_as_added
+    end
+  end
+
   resources 'searches'
 
   root to: 'products#index'
 
-
+  get 'about' => 'about_page#index', as: 'about'
+  get 'contact' => 'contact_page#index', as: 'contact'
+  get ':id' => 'products#by_category', as: 'categories'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
