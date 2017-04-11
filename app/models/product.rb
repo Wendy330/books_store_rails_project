bb618b98566d2dad21ad93fd1c8e244e41df55ef
Wendy330 <qian91328@gmail.com>
2017-04-11 22:51:03 +0000

    Add the model Line_Item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
   attr_accessor :remove_image
   belongs_to :category
+  has_many :line_items
   mount_uploader :image, ImageUploader
   validates :category_id, :name, :price, :description, :stock_quantity, presence:true
 
   def self.search(search)
-    where("name ILIKE ?", "%#{search}%") 
+    where("name ILIKE ?", "%#{search}%")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411214624) do
+ActiveRecord::Schema.define(version: 20170411220546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,17 @@ ActiveRecord::Schema.define(version: 20170411214624) do
     t.integer  "province_id"
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "quantity"
+    t.decimal  "price"
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id", using: :btree
+    t.index ["product_id"], name: "index_line_items_on_product_id", using: :btree
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string   "notes"
     t.decimal  "tax_rates"
@@ -116,6 +127,8 @@ ActiveRecord::Schema.define(version: 20170411214624) do
   end
 
   add_foreign_key "customers", "provinces"
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
   add_foreign_key "orders", "customers"
   add_foreign_key "products", "categories"
 end
