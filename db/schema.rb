8a82a40d862392fbd20c5954fe3b638630cc8bf2
Wendy330 <qian91328@gmail.com>
2017-04-13 01:37:17 +0000

    Remove status column from order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413011833) do
+ActiveRecord::Schema.define(version: 20170413013621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 20170413011833) do
   create_table "orders", force: :cascade do |t|
     t.string   "notes"
     t.decimal  "tax_rates"
-    t.string   "status"
     t.integer  "customer_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
