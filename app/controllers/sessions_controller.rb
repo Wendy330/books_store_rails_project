e4642cd7e5b17bb746ebe1cccd4e193e69c63d8d
Wendy330 <qian91328@gmail.com>
2017-04-14 20:19:30 +0000

    Add login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,19 @@ class SessionsController < ApplicationController
   def create
     customer = Customer.find_by(email: params[:session][:email].downcase)
     if customer && Customer.find_by(password:params[:session][:password].downcase)
-      # Log the user in and redirect to the user's show page.
+      # Log the user in and redirect to the home page.
+      session[:customer_id] = customer.id
+      redirect_to root_path
     else
       # Create an error message
-      flash[:danger] = 'Invalid email/password combination'
+      flash[:loginFail] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
+    session.delete(:customer_id)
+    @current_customer = nil
+    redirect_to root_path
   end
 end
